1ca50a78b387f70e7ab99c58d68cb90d7be17f20
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2022-01-22 03:15:11 +0200

    submit

---
+++ js/student.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+
+
+const addStudents = (id, username, gread) => {
+    const students = loadStudents();
+    console.log(gread);
+  
+    // duplicateId
+    const duplicateId = students.find((student) => {
+        return student.id === id;
+    });
+    console.log(duplicateId);
+    if (!duplicateId) {
+       
+        let sum =0;
+        gread.forEach((el) => {
+            sum = sum + el;
+        });
+        students.push({
+            id,
+            username,
+            gread,
+            sum
+
+
+
+
+        });
+        saveStudents(students);
+        console.log('student is saved successfuly');
+    } else {
+        console.log('Error duplicate id plase enter other id');
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+// delete 
+const deleteStudents = (id) => {
+    const students = loadStudents();
+    const studentsToKeep = students.filter((student) => {
+
+        return student.id !== id;
+    });
+    console.log(students.length);
+    console.log(studentsToKeep.length);
+
+    if (students.length !== studentsToKeep.length) {
+        saveStudents(studentsToKeep);
+        console.log('student is removed');
+    } else {
+        console.log('No student is found.. Please try again');
+    }
+
+};
+
+// /////////////////////////////////////////////////////////////////////////////////
+
+// // read
+
+const readStudents = (id) => {
+    const students = loadStudents();
+    const student = students.find((student) => {
+        return student.id === id;
+    });
+    console.log(student);
+    if (student) {
+        console.log(student.username);
+    } else {
+
+        console.log('No student is found');
+    }
+};
+
+const listStudents = () => {
+    const students = loadStudents();
+    students.forEach((student) => {
+        console.log(student.id, student.username, student.gread);
+    });
+};
+
+
+const loadStudents = () => {
+    try {
+        const dataBuffer = fs.readFileSync('students.json').toString();
+        return JSON.parse(dataBuffer);
+    } catch (e) {
+        return [];
+    }
+
+};
+
+const saveStudents = (student) => {
+    const saveData = JSON.stringify(student);
+    fs.writeFileSync('students.json', saveData);
+};
+
+module.exports = {
+    addStudents,
+    deleteStudents,
+    readStudents,
+    listStudents
+};
---
+++ js/test.js
@@ -0,0 +1,45 @@
+ let gread =[3,4];
+ let sum =0;
+// const top =gread.forEach(el=>{
+//     sum =sum + el;
+
+// });
+// console.log(sum);
+// gread.forEach(el => {
+//     let sum =0;
+//     sum = sum + el;
+//     return sum ;
+// })
+
+// function Sum(gread){
+//     let sum =0;
+//     for(let i= 0;i<gread.length;i++){
+//         sum = sum + gread[i];
+//     }
+//     console.log(sum);
+// }
+// Sum(gread);
+
+
+//  Sum = (gread) =>{
+//     gread.forEach((el)=>{
+//        return sum = sum + el;
+//     });
+// };
+// Sum(gread);
+
+
+
+
+// gread.forEach(el=>{
+//      sum = sum +el;
+// })
+// console.log(sum);
+
+
+
+
+gread.forEach((el) => {
+    sum = sum + el;
+})
+console.log(sum);
